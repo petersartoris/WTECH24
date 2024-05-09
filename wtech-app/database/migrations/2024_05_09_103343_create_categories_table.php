7ce9d93f1da953167afeca1281e4d8f4251ded8b
samuel-racak <samuel.racak@gmail.com>
2024-05-09 13:25:21 +0200

    Add migration for creating categories table

---
+++ wtech-app/database/migrations/2024_05_09_103343_create_categories_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps(); // Adds `created_at` and `updated_at` columns
+            $table->string('name', 16)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};

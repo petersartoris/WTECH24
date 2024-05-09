67fb3d770e5644e50a7feb66b896fb5b180921e9
samuel-racak <samuel.racak@gmail.com>
2024-05-09 13:26:24 +0200

    Add migration for creating products_categories table

---
+++ wtech-app/database/migrations/2024_05_09_110731_create_products_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_categories', function (Blueprint $table) {
+            // $table->id();
+            $table->foreignId('products_id')->constrained();
+            $table->foreignId('categories_id')->constrained();
+            $table->primary(['products_id', 'categories_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products_categories');
+    }
+};

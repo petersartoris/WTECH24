31ee49dd95bbd4309bfb6194ab4f78f379985d7e
samuel-racak <samuel.racak@gmail.com>
2024-05-10 13:46:20 +0200

    Add fillable attributes, relationships, and validation rules to DeliveryType model

--- wtech-app/app/Models/DeliveryType.php
+++ wtech-app/app/Models/DeliveryType.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class DeliveryType extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'price']; // The attributes that are mass assignable
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class); // A delivery type can have many orders
+    }
+
+    // validation rules for store and update methods
+    public static function rules($id = null)
+    {
+        return [
+            'name' => 'required|string|max:255|unique:delivery_types,name,' . $id . ',id',
+            'price' => 'required|numeric|min:0',
+        ];
+    }
 }

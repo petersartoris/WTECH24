7347a83e3eeb7c5072e7fd53047cdacccf716824
samuel-racak <samuel.racak@gmail.com>
2024-05-10 12:30:05 +0200

    Add unique name field to delivery_types table

--- wtech-app/database/migrations/2024_05_10_094446_create_delivery_types_table.php
+++ wtech-app/database/migrations/2024_05_10_094446_create_delivery_types_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('delivery_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }

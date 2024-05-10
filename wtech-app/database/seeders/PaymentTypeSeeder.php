2b3b73e4befe193f1435d76b191bc4dc0775a862
samuel-racak <samuel.racak@gmail.com>
2024-05-10 20:35:06 +0200

    Update PaymentTypeSeeder to use factory for creating payment types

--- wtech-app/database/seeders/PaymentTypeSeeder.php
+++ wtech-app/database/seeders/PaymentTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PaymentType;
 
 class PaymentTypeSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class PaymentTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // PaymentType::factory(3)->create(); // Create 3
+
+        // The code below is essentially the same as the code above as the factory already creates unique names for each PaymentType 
+        // PaymentType::factory()->create([
+        //     'name' => 'cash',
+        // ]);
+
+        // PaymentType::factory()->create([
+        //     'name' => 'credit card',
+        // ]);
+
+        // PaymentType::factory()->create([
+        //     'name' => 'paypal',
+        // ]);
     }
 }

f796942e48849e7a9b64a1a78da6aa440be92859
samuel-racak <samuel.racak@gmail.com>
2024-05-10 14:33:07 +0200

    Update payment_types table with a comment

--- wtech-app/database/migrations/2024_05_10_094100_create_payment_types_table.php
+++ wtech-app/database/migrations/2024_05_10_094100_create_payment_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('payment_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name')->unique(); // Name in ('credit card', 'paypal', 'cash on delivery')
             $table->timestamps();
         });
     }

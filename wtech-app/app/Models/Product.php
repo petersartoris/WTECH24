5d09fa5492dec509fed4c8f0a068cf2e82edb807
samuel-racak <samuel.racak@gmail.com>
2024-05-10 15:09:10 +0200

    Fix missing use for soft deletes

--- wtech-app/app/Models/Product.php
+++ wtech-app/app/Models/Product.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name', 'description', 'price', 'quantity', 'availability' ]; // The attributes that are mass assignable
+    protected $fillable = ['name', 'description', 'price', 'quantity', 'availability']; // The attributes that are mass assignable
 
     public function categories()
     {

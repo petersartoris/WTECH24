630f6c3643adb010abf10341aad6722672d07acb
samuel-racak <samuel.racak@gmail.com>
2024-05-10 14:32:20 +0200

    Update Product model with fillable attributes, relationships, and validation rules

--- wtech-app/app/Models/Product.php
+++ wtech-app/app/Models/Product.php
@@ -7,5 +7,66 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'price', 'quantity', 'availability' ]; // The attributes that are mass assignable
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class); // A product can belong to many categories
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)->withPivot('quantity'); // A product can belong to many orders
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class); // A product can have many images
+    }
+
+    public function getAvailabilityAttribute($value) // Get the availability of the product
+    {
+        return $value === 'available' ? 'available' : 'unavailable';
+    }
+
+    public function setAvailabilityAttribute($value) // Set the availability of the product
+    {
+        $this->attributes['availability'] = $value === 'available' ? 'available' : 'unavailable';
+    }
+
+    public function getPriceAttribute($value) // Get the price of the product
+    {
+        return number_format($value, 2);
+    }
+
+    public function setPriceAttribute($value) // Set the price of the product
+    {
+        $this->attributes['price'] = number_format($value, 2);
+    }
+
+    public function getQuantityAttribute($value) // Get the quantity of the product
+    {
+        return number_format($value);
+    }
+
+    public function setQuantityAttribute($value) // Set the quantity of the product
+    {
+        $this->attributes['quantity'] = number_format($value);
+    }
+
+    // validation rules for store and update methods
+    public static function rules($id = null)
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+            'availability' => 'required|string|in:available,unavailable',
+            'categories' => 'required|array',
+            'categories.*' => 'exists:categories,id',
+        ];
+    }
 }

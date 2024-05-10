adb46319629af261bb987bf734010b613ae2dead
samuel-racak <samuel.racak@gmail.com>
2024-05-10 13:50:10 +0200

    Add price field to delivery_types table

--- wtech-app/database/migrations/2024_05_10_094446_create_delivery_types_table.php
+++ wtech-app/database/migrations/2024_05_10_094446_create_delivery_types_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('delivery_types', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }

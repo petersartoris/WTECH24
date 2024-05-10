36031c2b6bcc5d85fd672b21d11bbd176aab8ae6
samuel-racak <samuel.racak@gmail.com>
2024-05-10 12:29:54 +0200

    Add name to payment_type

--- wtech-app/database/migrations/2024_05_10_094100_create_payment_types_table.php
+++ wtech-app/database/migrations/2024_05_10_094100_create_payment_types_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('payment_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }

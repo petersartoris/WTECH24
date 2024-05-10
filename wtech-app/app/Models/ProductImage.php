007aec562257214bf9e70789d69e929b98f52781
samuel-racak <samuel.racak@gmail.com>
2024-05-10 14:32:29 +0200

    Update ProductImage model with fillable attributes, relationships, and validation rules

--- wtech-app/app/Models/ProductImage.php
+++ wtech-app/app/Models/ProductImage.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id', 'path']; // The attributes that are mass assignable
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class); // A product image belongs to a product
+    }
+
+    // validation rules for store and update methods
+    public static function rules($id = null)
+    {
+        return [
+            'product_id' => 'required|exists:products,id',
+            'path' => 'required|string|max:255',
+        ];
+    }
 }

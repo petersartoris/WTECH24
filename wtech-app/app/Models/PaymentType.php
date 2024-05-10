8d9b8cc0e61d6e7dcf51ec6041c0fc1f0a678da2
samuel-racak <samuel.racak@gmail.com>
2024-05-10 14:32:07 +0200

    Update PaymentType model with fillable attributes, relationships, and validation rules

--- wtech-app/app/Models/PaymentType.php
+++ wtech-app/app/Models/PaymentType.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentType extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name']; // The attributes that are mass assignable
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class); // A payment type can have many orders
+    }
+
+    // validation rules for store and update methods
+    public static function rules($id = null)
+    {
+       return [
+        'name' => 'required|string|max:255|in:credit card,paypal,cash on delivery|unique:payment_types,name,' . $id . ',id',
+       ];
+    }
 }

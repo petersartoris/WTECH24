98ae1c5a0f00cfe6b1169758ec271333039a4be1
samuel-racak <samuel.racak@gmail.com>
2024-05-10 13:07:25 +0200

    Removed an empty line

--- wtech-app/database/migrations/2024_05_10_094750_create_categories_table.php
+++ wtech-app/database/migrations/2024_05_10_094750_create_categories_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade'); // If parent category is deleted, the child category will also be deleted
             $table->string('name'); // Name
-
             $table->unique(['parent_id', 'name']); // A category name must be unique within the same parent category
             // $table->softDeletes(); // Soft delete column
             $table->timestamps();

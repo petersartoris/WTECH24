655c11ab7cb8ab91f29bba60a5a6be8a8c5034e3
samuel-racak <samuel.racak@gmail.com>
2024-05-11 13:33:07 +0200

    Add unique product code in Product model and migration

--- wtech-app/database/factories/ProductFactory.php
+++ wtech-app/database/factories/ProductFactory.php
@@ -18,6 +18,7 @@ class ProductFactory extends Factory
     {
         return [
             'name' => $this->faker->unique()->word(),
+            'code' => $this->faker->unique()->bothify('product-###?###'), // Add this line
             'description' => $this->faker->sentence(),
             'price' => $this->faker->randomFloat(2, 1, 1000),
             'quantity' => $this->faker->numberBetween(1, 100),

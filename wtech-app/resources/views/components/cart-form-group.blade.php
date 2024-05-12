e0458485b5a1cf449f4a484e7ccf055912270f78
samuel-racak <samuel.racak@gmail.com>
2024-05-12 22:47:20 +0200

    Update shopping delivery address form with hidden fields and fix formatting

--- wtech-app/resources/views/components/cart-form-group.blade.php
+++ wtech-app/resources/views/components/cart-form-group.blade.php
@@ -1,4 +1,5 @@
 <div class="form-group pb-2">
     <label for="{{ $id }}" class="pb-1">{{ $label }}</label>
-    <input type="{{ $type }}" class="form-control" id="{{ $id }}" autocomplete="{{ $autocomplete }}">
+    <input type="{{ $type }}" class="form-control" id="{{ $id }}" name="{{ $name }}"
+        autocomplete="{{ $autocomplete }}">
 </div>
--- wtech-app/resources/views/shopping-delivery-address.blade.php
+++ wtech-app/resources/views/shopping-delivery-address.blade.php
@@ -9,30 +9,39 @@
         <section id="delivery-page">
             <div class="container container-custom">
                 <div class="row">
-                    <div class="col-md-7 col-sm-12 mt-3">
-                        <h2 class="text-center border-custom" id="delivery-address">Enter order information</h2>
-                        <form method="POST" action="{{ route('order-create') }}">
-                            @csrf
-                            <x-cart-form-group id="name" label="First name, Last name" type="text" autocomplete="name"></x-cart-form-group>
-                            <x-cart-form-group id="address" label="Street address, Postal code" type="text" autocomplete="street-address"></x-cart-form-group>
-                            <x-cart-form-group id="city" label="City, Country" type="text" autocomplete="address-level2"></x-cart-form-group>
-                            <x-cart-form-group id="phone" label="Phone number" type="tel" autocomplete="tel"></x-cart-form-group>
-                        
-                            <div class="d-flex flex-column align-items-center">
-                                <h2 class="white-text mt-4">Total: {{ $total }} €</h2>
-                                <button type="submit" id="finish-order" class="button-custom button-red mt-2">Finish Order</button>
-                            </div>
-                        </form>
+                    <form method="POST" action="{{ route('order-create') }}">
+                        @csrf
+                        <x-cart-form-group id="name" name="name" label="First name, Last name" type="text"
+                            autocomplete="name"></x-cart-form-group>
+                        <x-cart-form-group id="address" name="address" label="Street address, Postal code"
+                            type="text" autocomplete="street-address"></x-cart-form-group>
+                        <x-cart-form-group id="city" name="city" label="City, Country" type="text"
+                            autocomplete="address-level2"></x-cart-form-group>
+                        <x-cart-form-group id="phone" name="phone" label="Phone number" type="tel"
+                            autocomplete="tel"></x-cart-form-group>
 
-                    </div>
+                        <!-- Add these hidden fields -->
+                        <input type="hidden" name="total" value="{{ $total }}">
+                        <input type="hidden" name="deliveryMethod" value="{{ $deliveryMethod }}">
+                        <input type="hidden" name="paymentMethod" value="{{ $paymentMethod }}">
+
+                        <div class="d-flex flex-column align-items-center">
+                            <h2 class="white-text mt-4">Total: {{ $total }} €</h2>
+                            <button type="submit" id="finish-order" class="button-custom button-red mt-2">Finish
+                                Order</button>
+                        </div>
+                    </form>
+
+
 
                     <div class="col-md-5 col-sm-12 mt-5 mt-md-3 align-content-center">
                         <div class="picture-container">
-                            <img class="picture img-fluid mw-100" src="{{ asset('images/delivery-address/delivery-page.jpeg') }}"
+                            <img class="picture img-fluid mw-100"
+                                src="{{ asset('images/delivery-address/delivery-page.jpeg') }}"
                                 alt="picture of mascot in storage facility">
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </section>
